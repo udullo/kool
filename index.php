2f892c489861d670509becb294917616893489f7
Tarmo Teder <Tarmo.Teder@tptlive.ee>
2018-01-03 13:58:25 +0200

    Commit koos tingimuslausega

--- index.php
+++ index.php
@@ -7,15 +7,20 @@ $nr = 1;
 // Kümnendmurd
 $nr = 10.0121;
 // Tõeväärtusmuutuja
-$bool = False;
+$bool = True;
 /*
 Massiiv (php ja JS - array, python - list)
 Üherealine kommentaar on // ja #
 */
 $massiiv = array('Nimi',1974,10.81,'Perenimi' );
 
-
 echo "$tervitus world!<br>";
 echo '$tervitus world!<br>';
 echo $massiiv[0];
+
+// Tingimuslause
+if ($bool == False){
+  echo $massiiv;
+} else { echo "Tõeväärtus ei vasta nõutule"; }
+
  ?>
