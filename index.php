939764ba9a80d5a31202c9c912d0156a5942de5c
Tarmo Teder <Tarmo.Teder@tptlive.ee>
2018-01-03 12:59:57 +0200

    Esimene commit, 03.01.2018, index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// Tekstimuutuja
+$tervitus = 'Hello';
+# Täisar4vuline muutuja
+$nr = 1;
+// Kümnendmurd
+$nr = 10.0121;
+// Tõeväärtusmuutuja
+$bool = False;
+/*
+Massiiv (php ja JS - array, python - list)
+Üherealine kommentaar on // ja #
+*/
+$massiiv = array('Nimi',1974,10.81,'Perenimi' );
+
+
+echo "$tervitus world!<br>";
+echo '$tervitus world!<br>';
+echo $massiiv[0];
+ ?>
