3ff3fa31be479efa3b3b88300b6fd379be9b2f2b
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-22 14:11:01 +0200

    sidusmassiiv ja faili lugemine/kirjutamine

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<meta charset="UTF-8" />
+
 <?php
 
 // Tekstimuutuja
@@ -14,9 +16,19 @@ Massiiv (php ja JS - array, python - list)
 */
 $massiiv = array('Nimi',1974,10.81,'Perenimi' );
 
+$esta = 'Eesti keel';
+$mat = 'Matemaatika';
+$fyss = 'Füüsika';
+
+//sidusmassiivid
+$hinded = array('Peeter'=>array( $esta =>4, $mat =>4, $fyss =>3),
+                'Mari'=>array( $esta =>4, $mat =>4, $fyss =>3),
+                'Jüri'=>array( $esta =>4, $mat =>4, $fyss =>3));
+
+
 echo "$tervitus world!<br>";
 echo '$tervitus world!<br>';
-echo $massiiv[0].'<br>';
+//echo $massiiv[0].'<br>''
 
 // Tingimuslause
 if ($bool == False){
@@ -27,4 +39,28 @@ echo $massiiv[$i].'<br>';
 }
 } else { echo "Tõeväärtus ei vasta nõutule"; }
 
- ?>
+$hinded_id = array_keys($hinded);
+for ($i=0; $i < count($hinded); $i++ ){
+  echo $hinded_id[$i].'<br>';
+  foreach ($hinded[$hinded_id[$i]] as $aine => $hinne) {
+    echo $aine." : ".$hinne.'<br>';
+  }}
+
+$file = fopen('counter.txt', "a") or die("Ei saanud faili avada");
+if(!$file){
+$txt = 1;
+fwrite($file,$txt);
+fclose($file);
+} else {
+  $txt =fread($file, filesize('counter.txt'));
+  $new_txt = $txt + 1
+  fwrite($file,$new_txt);
+  fclose($file);
+}
+
+$file = fopen('counter.txt', "r") or die("Ei saanud faili avada");
+echo fread($file, filesize('counter.txt'));
+fclose($file);
+
+
+?>
